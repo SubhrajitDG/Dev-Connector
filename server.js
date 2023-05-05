60dcadb7df07d0bbe4a2228ddb35c7dc2d644bc4
Akash Dasgupta <bloombergakash@gmail.com>
2023-05-05 20:20:48 +0530

    cors fix

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const path = require('path');
-const { resolve } = require('path');
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+const { resolve } = require("path");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,21 +10,22 @@ const app = express();
 connectDB();
 
 //init MiddleWare
+app.use(cors()); // Add CORS middleware
 app.use(express.json({ extended: false }));
 
 //Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/post', require('./routes/api/post'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path, resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path, resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
